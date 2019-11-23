be915474bedc39dd5c714aac73011de44ce3ffd8
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-11-23 13:54:21 -0500

    add service and buttons logic

---
+++ Q50Remote/Q50Remote/Services/AutoPiService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q50Remote.Services
+{
+    public class AutoPiService
+    {
+        HttpClient _httpClient;
+        public AutoPiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task SendCommand()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
--- Q50Remote/Q50Remote/ViewModels/MainPageViewModel.cs
+++ Q50Remote/Q50Remote/ViewModels/MainPageViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using Q50Remote.Enums;
+using Q50Remote.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Q50Remote.ViewModels
@@ -11,19 +13,41 @@ namespace Q50Remote.ViewModels
     public class MainPageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        public Command<string> SendCarCommand { get; }
-        public Command Unlock { get; }
-        //public ICommand Lock { get; }
-        //public ICommand Lock { get; }
-        //public ICommand Lock { get; }
+        private bool _carCommandNotRunning = true;
+        public bool CarCommandNotRunning
+        {
+            get => _carCommandNotRunning;
+            set
+            {
+                _carCommandNotRunning = value;
+                OnPropertyChanged(nameof(CarCommandNotRunning));
+            }
+        }
+        public Command<CarCommandEnum> SendCarCommand { get; }
+        private AutoPiService _autoPiService { get; }
 
         public MainPageViewModel()
         {
-            SendCarCommand = new Command<string>((carCommand) =>
+            _autoPiService = new AutoPiService();
+
+            SendCarCommand = new Command<CarCommandEnum>(async(carCommand) =>
             {
-                var name = Enum.GetName(typeof(CarCommandEnum), carCommand);
+                CarCommandNotRunning = false;
+
+                string name = Enum.GetName(typeof(CarCommandEnum), carCommand);
                 Debug.WriteLine(name);
+
+                await _autoPiService.SendCommand();
+
+                Debug.WriteLine(name);
+
+                CarCommandNotRunning = true;
             });
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
