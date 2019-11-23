177fde256b656eb4772b4e48a9bd0a713e8a6a6e
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-11-23 12:43:37 -0500

    Add project files.

---
+++ Q50Remote/Q50Remote/Enums/CarCommandEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q50Remote.Enums
+{
+    public enum CarCommandEnum
+    {
+        Lock = 1,
+        Unlock = 2,
+        Trunk = 3,
+        Alarm = 4,
+        Windows = 5,
+        Location = 6
+    }
+}
---
+++ Q50Remote/Q50Remote/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Q50Remote.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Q50Remote.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Command<string> SendCarCommand { get; }
+        public Command Unlock { get; }
+        //public ICommand Lock { get; }
+        //public ICommand Lock { get; }
+        //public ICommand Lock { get; }
+
+        public MainPageViewModel()
+        {
+            SendCarCommand = new Command<string>((carCommand) =>
+            {
+                var name = Enum.GetName(typeof(CarCommandEnum), carCommand);
+                Debug.WriteLine(name);
+            });
+        }
+    }
+}
