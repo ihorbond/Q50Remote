692feb17742fffc9d3b2c116c14225b8b3c75c46
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-11-24 19:44:10 -0500

    add show app info button

--- Q50Remote/Q50Remote/ViewModels/SettingsPageViewModel.cs
+++ Q50Remote/Q50Remote/ViewModels/SettingsPageViewModel.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Q50Remote.ViewModels
 {
     public class SettingsPageViewModel: SettingsPageBaseViewModel
     {
+        public Command ShowAppInfoCommand { get; }
+
         public SettingsPageViewModel()
         {
+            ShowAppInfoCommand = new Command(() =>
+            {
+                AppInfo.ShowSettingsUI();
+            });
         }
 
     }
